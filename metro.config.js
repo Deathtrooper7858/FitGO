05ab8f696c7de0d987aaa93c6e54357639d01281
Deathtrooper <deathtrooper7858@gmail.com>
2026-05-27 21:28:36 -0500

    fix

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Prevenir caídas del vigilante de archivos (ENOENT) en Windows
+// ignorando las carpetas de compilación de Android/iOS
+config.resolver.blockList = [
+  ...Array.from(config.resolver.blockList || []),
+  /.*\/android\/.*/,
+  /.*\/ios\/.*/
+];
+
 module.exports = config;
